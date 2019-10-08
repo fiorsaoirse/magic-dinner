bfd9ac4e29c61ae91a509d0d659abaf41e150bb7
fiorsaoirse <solovyanovamaria@yandex.ru>
2019-10-08 17:43:33 +0300

    Test refactor.

--- rest/__tests__/generateAST.test.js
+++ rest/__tests__/generateAST.test.js
@@ -9,44 +9,36 @@ const oneElement = 'one-element.txt';
 const html = 'mock-get-page.txt';
 const htmlWithDefault = 'mock-get-page-def.txt';
 
-test('Parse HTML with one element and generate AST', async () => {
-  const promise = new Promise((res, rej) => {
-    fs.readFile(path.join(__dirname, fixtures, oneElement), 'utf-8', (err, file) => {
-      if (err) rej(err);
-      res(file);
+const slicedData = data.slice(0, 1);
+
+class TestData {
+  constructor(description, source, expected) {
+    this.description = description;
+    this.source = source;
+    this.expected = expected;
+  }
+}
+
+const testData = [
+  new TestData('Parse HTML with one element and generate AST', oneElement, slicedData),
+  new TestData('Parse HTML with some recipes and generate AST', html, data),
+  new TestData('Parse HTML with some recipes which doesn\'t have images and generate AST', htmlWithDefault, dataWithDefaults),
+];
+
+describe('Testing of generating AST', () => {
+  testData.forEach(({ description, source, expected }) => {
+    test(description, async () => {
+      const promise = new Promise((res, rej) => {
+        fs.readFile(path.join(__dirname, fixtures, source), 'utf-8', (err, file) => {
+          if (err) rej(err);
+          res(file);
+        });
+      });
+
+      const snapshot = await promise;
+      const result = ast(snapshot);
+
+      expect(result).toEqual(expected);
     });
   });
-
-  const snapshot = await promise;
-  const result = ast(snapshot);
-
-  expect(result).toEqual(data.slice(0, 1));
-});
-
-test('Parse HTML with some recipes and generate AST', async () => {
-  const promise = new Promise((res, rej) => {
-    fs.readFile(path.join(__dirname, fixtures, html), 'utf-8', (err, file) => {
-      if (err) rej(err);
-      res(file);
-    });
-  });
-
-  const snapshot = await promise;
-  const result = ast(snapshot);
-
-  expect(result).toEqual(data);
-});
-
-test('Parse HTML with some recipes which doesn\'t have images and generate AST', async () => {
-  const promise = new Promise((res, rej) => {
-    fs.readFile(path.join(__dirname, fixtures, htmlWithDefault), 'utf-8', (err, file) => {
-      if (err) rej(err);
-      res(file);
-    });
-  });
-
-  const snapshot = await promise;
-  const result = ast(snapshot);
-
-  expect(result).toEqual(dataWithDefaults);
 });
