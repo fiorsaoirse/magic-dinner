339283898a4c2e8b73ceda46a9da51c5fa0fb18f
fiorsaoirse <solovyanovamaria@yandex.ru>
2020-10-17 13:39:31 +0300

    Refactoring of code.

--- src/parseRecipe.js
+++ src/parseRecipe.js
@@ -1,53 +1,51 @@
 import cheerio from 'cheerio';
 import {
-    findNodeByPredicate, isTagNode, isTextNode, reduceNode,
+    findNodeByPredicate, isTagNode,
+    isTextNode, reduceNode,
 } from './utils/nodesUtils';
 import Recipe from './entities/classes/recipe';
 import Ingredient from './entities/classes/ingredient';
 import getEnergyCalculating from './utils/parseEnergy';
 import formatChars from './utils/charsFormatter';
 
-// todo: fix
 const extractImage = (container) => {
-    const imageNode = findNodeByPredicate((currentNode) => {
-        if (!isTagNode(currentNode)) {
+    const imageContainerPredicate = ((node) => {
+        if (!isTagNode(node)) {
             return false;
         }
-        const { name, parent: { attribs: { class: classList } } } = currentNode;
+        const { name, parent: { attribs: { class: classList } } } = node;
         const isPreviewNode = classList && /preview/gm.test(classList);
         return !isPreviewNode && (name === 'img' || name === 'image');
-    }, container);
+    });
+    const imageNode = findNodeByPredicate(imageContainerPredicate, container);
     return imageNode?.attribs?.src || imageNode?.attribs?.href || null;
 };
 
-// todo: fix
 const extractPortions = (containers) => containers.reduce((acc, node) => {
-    const result = reduceNode((nAcc, currentNode) => {
-        if (isTextNode(currentNode) && currentNode.parent.name === 'span'
-            && currentNode.parent.attribs.class.includes('js-portions-count-print')) {
-            return currentNode.data.trim();
-        }
-        return nAcc;
-    }, node, []);
-    return [ ...acc, result ];
+    const getPortion = (iAcc, iNode) => {
+        const parentNode = iNode.parent;
+        const isNecessaryNode = isTextNode(iNode) && parentNode?.name === 'span'
+            && parentNode?.attribs.class.includes('js-portions-count-print');
+        return isNecessaryNode ? iNode.data.trim() : iAcc;
+    };
+    const portionData = reduceNode(getPortion, node, []);
+    return [ ...acc, portionData ];
 }, []).join('');
 
-// todo: fix
 const extractInstructions = (containers) => containers.map(({ children }) => {
-    const mappedChildren = children.map((child) => reduceNode((innerAcc, innerNode) => {
-        let newInnerAcc = [ ...innerAcc ];
-        if (isTextNode(innerNode)) {
-            const data = innerNode.data.trim();
-            if (!!data && !data.match(/^(\d+\.)/)) {
-                newInnerAcc = [ ...newInnerAcc, formatChars(innerNode.data.trim()) ];
-            }
+    const getInstruction = (acc, node) => {
+        if (!isTextNode(node)) {
+            return acc;
         }
-        return newInnerAcc;
-    }, child, []));
+        const data = node?.data.trim();
+        const digitOfStepPattern = /^(\d+\.)/;
+        const isDataDigitOfStep = data && !data.match(digitOfStepPattern);
+        return isDataDigitOfStep ? [ ...acc, formatChars(data)] : acc;
+    };
+    const mappedChildren = children.map((child) => reduceNode(getInstruction, child, []));
     return mappedChildren.reduce((acc, child) => [ ...acc, ...child ]).join(' ');
 });
 
-// todo: fix
 const extractEnergy = (containers) => containers.reduce((acc, nutrition) => {
     const childTags = nutrition.children.filter((child) => isTagNode(child));
     const typeNode = childTags.find((child) => child.attribs.class.includes('nutrition__name'));
@@ -57,7 +55,6 @@ const extractEnergy = (containers) => containers.reduce((acc, nutrition) => {
     return getValue(acc, valueNode);
 }, {});
 
-// todo: fix
 const extractIngredients = (containers) => containers.reduce((acc, curr) => {
     const dataIngredient = curr.attribs['data-ingredient-object'];
     if (!dataIngredient?.length) return acc;
@@ -72,9 +69,8 @@ export default (html) => {
     const titleContainer = dom('h1.recipe__name');
     const imageContainer = dom('div.recipe__cover').get(0);
     const infoContainer = dom('div.recipe__info-pad');
-    // todo: fix?
-    const nutritionListContainer = dom('ul.nutrition__list');
-    const nutritionList = (nutritionListContainer.length !== 0 && nutritionListContainer?.children()) || [];
+    const nutritionList = dom('ul.nutrition__list');
+    const nutritionListContainer = (nutritionList.length !== 0 && nutritionList?.children()) || [];
     const ingredientsList = dom('div.ingredients-list.layout__content-col > div.ingredients-list__content >'
         + ' p.content-item');
     const stepsContainer = dom('span.instruction__description');
@@ -82,7 +78,7 @@ export default (html) => {
     const recipeTitle = titleContainer && titleContainer.text().trim();
     const portions = extractPortions(Array.from(infoContainer));
     const instructions = extractInstructions(Array.from(stepsContainer));
-    const energy = extractEnergy(Array.from(nutritionList));
+    const energy = extractEnergy(Array.from(nutritionListContainer));
     const ingredients = extractIngredients(Array.from(ingredientsList));
     return new Recipe(formatChars(recipeTitle), instructions, portions, ingredients, energy, image);
 };
